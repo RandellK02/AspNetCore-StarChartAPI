c4c70af75420b321115e9c06e8966c38c95b8233
Randell Koen <Randell.Koen@Waterboards.ca.gov>
2021-10-19 16:36:04 -0700

    M3.2

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,9 +9,10 @@ namespace StarChart.Models
 {
     public class CelestialObject
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public int? OrbitedObjectID { get; set; }
+        public int? OrbitedObjectId { get; set; }
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
